f1882cb37d005f9643b3be9e71f0a9d2bc953608
Ozlem <cse.ozlemkorpe@gmail.com>
2020-06-23 04:02:29 +0300

    Decision Tree Guide Added

--- Decision_Trees_Guide.m
+++ Decision_Trees_Guide.m
@@ -1,37 +1,19 @@
-% Naive Bayes Algorithm
+% Decision Tree Algorithm
 
-% P(A|B) = (P(B|A) x P(A)) / P(B)
-% P(A) = Occurence of A / Total
-
-clear all
-
-%% --------------- Importing the dataset -------------------------
-% ---------------------------- Code ---------------------------
+%---------------Importing Dataset
 data = readtable('C:\Users\Asus\Desktop\necessary\Naive Bayes\Social_Network_Ads.csv');
 
-
-
-%________________________________________________________________
-%________________________________________________________________
-
-%%---------------Data Preprocessing -----------------------------
-
-
-%% -------------- Feature Scalling -------------------------------
-
-% -------------- Method 1: Standardization ----------------------
-% ---------------------------- Code -----------------------------
-
+%---------------Feature Scaling (Standardization Method)
 stand_age = (data.Age - mean(data.Age))/std(data.Age);
 data.Age = stand_age; 
 
 stand_estimted_salary = (data.EstimatedSalary - mean(data.EstimatedSalary))/std(data.EstimatedSalary);
 data.EstimatedSalary = stand_estimted_salary; 
 
- 
 %---------------Classifying Data  
-classification_model = fitcnb(data,'Purchased~Age+EstimatedSalary');
-
+classification_model = fitctree(data,'Purchased~Age+EstimatedSalary');
+% Uses normal distribution as default
+% classification_model = fitcnb(normalized_data, 'Survived~Age+Fare+Parch+SibSp+female+male+Pclass','Distribution','kernel');
 
 %---------------Partitioning
 cv = cvpartition(classification_model.NumObservations, 'HoldOut', 0.2);
@@ -43,10 +25,9 @@ Predictions = predict(cross_validated_model.Trained{1},data(test(cv),1:end-1));
 %---------------Analyzing the Results
 Results = confusionmat(cross_validated_model.Y(test(cv)),Predictions);
 
-%---------------Visualizing the Results
+%---------------Visualizing Training Results
 labels = unique(data.Purchased);
-classifier_name = 'Naive Bayesain (Training Results)';
-
+classifier_name = 'Decision Tree(Training Results)';
 
 Age_range = min(data.Age(training(cv)))-1:0.01:max(data.Age(training(cv)))+1;
 Estimated_salary_range = min(data.EstimatedSalary(training(cv)))-1:0.01:max(data.EstimatedSalary(training(cv)))+1;
@@ -63,10 +44,8 @@ hold on
 training_data = data(training(cv),:);
 Y = ismember(training_data.Purchased,labels{1});
  
- 
 scatter(training_data.Age(Y),training_data.EstimatedSalary(Y), 'o' , 'MarkerEdgeColor', 'black', 'MarkerFaceColor', 'red');
 scatter(training_data.Age(~Y),training_data.EstimatedSalary(~Y) , 'o' , 'MarkerEdgeColor', 'black', 'MarkerFaceColor', 'green');
- 
  
 xlabel('Age');
 ylabel('Estimated Salary');
@@ -76,14 +55,9 @@ legend off, axis tight
  
 legend(labels,'Location',[0.45,0.01,0.45,0.05],'Orientation','Horizontal');
  
- 
- 
-%% -------------- Visualizing testing set results ----------------
-% ---------------------------- Code ---------------------------
- 
+%---------------Visualizing Test Results 
 labels = unique(data.Purchased);
-classifier_name = 'K-Nearest Neigbor (Testing Results)';
- % please mention your classifier name here
+classifier_name = 'Decision Tree (Testing Results)';
 
 Age_range = min(data.Age(training(cv)))-1:0.01:max(data.Age(training(cv)))+1;
 Estimated_salary_range = min(data.EstimatedSalary(training(cv)))-1:0.01:max(data.EstimatedSalary(training(cv)))+1;
@@ -105,7 +79,6 @@ Y = ismember(testing_data.Purchased,labels{1});
 scatter(testing_data.Age(Y),testing_data.EstimatedSalary(Y), 'o' , 'MarkerEdgeColor', 'black', 'MarkerFaceColor', 'red');
 scatter(testing_data.Age(~Y),testing_data.EstimatedSalary(~Y) , 'o' , 'MarkerEdgeColor', 'black', 'MarkerFaceColor', 'green');
 
-
 xlabel('Age');
 ylabel('Estimated Salary');
 
@@ -113,7 +86,4 @@ title(classifier_name);
 legend off, axis tight
 
 legend(labels,'Location',[0.45,0.01,0.45,0.05],'Orientation','Horizontal');
- 
-%________________________________________________________________
-%________________________________________________________________
  
