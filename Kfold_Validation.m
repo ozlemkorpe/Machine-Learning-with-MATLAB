d3fe392c703107964648cd772f1ab535be18b5d3
Ozlem <cse.ozlemkorpe@gmail.com>
2020-07-06 06:13:47 +0300

    Predictions for KFold validation

--- Kfold_Validation.m
+++ Kfold_Validation.m
@@ -40,15 +40,24 @@ classification_model.NumNeighbors = 5; %Change number of neighbours.
 
 %Validation with Holdout and Kfold
 %cv = cvpartition(classification_model.NumObservations,'HoldOut', 0.2); %Built-in function for partitioning
-cv = cvpartition(classification_model.NumObservations,'Kfold', 5);
+cv = cvpartition(classification_model.NumObservations,'KFold', 5); %Produce 5 classifiers
 cross_validated_model = crossval(classification_model, 'cvpartition', cv); %Use training set only to built model 
 
-%-----------Make predictions for the testing set
-Predictions = predict(cross_validated_model.Trained{1}, data(test(cv),1:end-1));
+%-----------Make predictions for the each testing set
+Predictions_K1 = predict(cross_validated_model.Trained{1}, data(test(cv,1),1:end-1));
+Predictions_K2 = predict(cross_validated_model.Trained{1}, data(test(cv,2),1:end-1));
+Predictions_K3 = predict(cross_validated_model.Trained{1}, data(test(cv,3),1:end-1));
+Predictions_K4 = predict(cross_validated_model.Trained{1}, data(test(cv,4),1:end-1));
+Predictions_K5 = predict(cross_validated_model.Trained{1}, data(test(cv,5),1:end-1));
 
 %-----------Analyzing the predictions
-    %Confusion Matrix: / diagonal will give the false predictions, \ will
-    %be the rigth predictions.
-Results = confusionmat(cross_validated_model.Y(test(cv)),Predictions); 
+    %Confusion Matrix for each test results
+Results_K1 = confusionmat(cross_validated_model.Y(test(cv,1)),Predictions_K1); 
+Results_K2 = confusionmat(cross_validated_model.Y(test(cv,2)),Predictions_K2); 
+Results_K3 = confusionmat(cross_validated_model.Y(test(cv,3)),Predictions_K3); 
+Results_K4 = confusionmat(cross_validated_model.Y(test(cv,4)),Predictions_K4); 
+Results_K5 = confusionmat(cross_validated_model.Y(test(cv,5)),Predictions_K5); 
 %data.Purchased(test(cv));
 
+%Combine results in  one confusion matrix
+Results = Results_K1 + Results_K2 + Results_K3 + Results_K4 + Results_K5
