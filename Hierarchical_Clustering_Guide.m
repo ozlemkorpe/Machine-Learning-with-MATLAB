f0ebad01e0b94ee66e5946a0d86a4cac06a7bea9
Ozlem <cse.ozlemkorpe@gmail.com>
2020-08-10 21:09:57 +0300

    Created hierarchical clustering

---
+++ Hierarchical_Clustering_Guide.m
@@ -0,0 +1,11 @@
+%%%------------- Hierarchical Clustering
+% Step 1: Consider each data point as a single cluster.
+% Step 2: Combine the two closest clusters and make them one cluster.
+% Step 3: Repeatedly combine clusters until there is only one cluster.
+
+% Approaches for finding closest clusters
+% Single Link: Min distance
+% Complete Link: Max distance
+% Average: Average distance
+
+% Types of Hierachical Clustering : Agglomerative, Divisive
