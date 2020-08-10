95e6d096aeef60ea3c9f5cf3f3a21b38e6aa3956
Ozlem <cse.ozlemkorpe@gmail.com>
2020-08-10 21:30:49 +0300

    Update Hierarchical_Clustering_Guide.m

--- Hierarchical_Clustering_Guide.m
+++ Hierarchical_Clustering_Guide.m
@@ -9,3 +9,44 @@
 % Average: Average distance
 
 % Types of Hierachical Clustering : Agglomerative, Divisive
+
+% Import the dataset
+data = readtable('Datasets\Mall_Customers.csv');
+
+%Check for missing values
+missings = sum(ismissing(data));
+
+%Plot variables to check for outliers
+IncomePlot = plot(data.AnnualIncome);
+SpendingPlot = plot(data.SpendingScore);
+
+% Perform Feature Scaling (Standardization Method)
+stand_income = (data.AnnualIncome - mean(data.AnnualIncome)) / std(data.AnnualIncome);
+data.AnnualIncome = stand_income; 
+
+stand_spending = (data.SpendingScore - mean(data.SpendingScore)) / std(data.SpendingScore);
+data.SpendingScore = stand_spending; 
+
+% Select columns for clustering
+selected_data = data(:,4:5);
+
+%Data must be an array to be used in clustering algorithm
+arrayed_data = table2array(selected_data);
+
+
+
+%------- Visualization
+data = arrayed_data;
+figure,
+
+gscatter(data(:,1),data(:,2),idx);
+hold on
+
+for i=1:6
+    scatter(C(i,1),C(i,2),96,'black','filled');
+end
+
+legend({'Cluster 1', 'Cluster 2', 'Cluster 3', 'Cluster 4', 'Cluster 5','Cluster 6' })
+xlabel('Annual Income');
+ylabel('Spending Score');
+hold off
